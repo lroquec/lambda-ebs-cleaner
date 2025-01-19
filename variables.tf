18d567ae00cb907bd944e9f8b0ac77fc385f36bc
Laura Roque <lroquecu@gmail.com>
2025-01-19 13:57:35 +0100

    variables

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  description = "The region in which the resources will be created."
+  default     = "us-east-1"
+}
+
+variable "lambda_name" {
+  description = "The name to be used for the Lambda function."
+  type        = string
+  default     = "test_apigw_integration"
+}
+
+variable "lambda_log_retention" {
+  description = "The number of days to retain the logs for the Lambda function."
+  type        = number
+  default     = 7
+}
