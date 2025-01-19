6ac658a9e1d445dcea28eca92c60e2d97d561044
Laura Roque <lroquecu@gmail.com>
2025-01-19 14:00:15 +0100

    datasources

---
+++ datasources.tf
@@ -0,0 +1,9 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  output_path = "${path.module}/src.zip"
+  source_dir  = "${path.module}/src"
+}
